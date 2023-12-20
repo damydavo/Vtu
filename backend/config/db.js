8d9a9ab658d01a89065df65b313ac41ba5ddf809
unknown <damydavo2015@gmail.com>
2023-12-20 12:13:27 +0100

    added the project

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,8 +2,14 @@ import mongoose from "mongoose";
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI);
-        console.log(`MongoDB Connected: ${conn.connection.host}`);
+        // const conn = await mongoose.connect(process.env.MONGO_URI);
+        // console.log(`MongoDB Connected: ${conn.connection.host}`);
+        mongoose.connect('mongodb+srv://damydavo:electelect123@damydavocluster.yvjol.mongodb.net/sunking?retryWrites=true&w=majority', { useNewUrlParser: true });
+        mongoose.connection.once('open', function () {
+            console.log('Conection has been made!');
+        }).on('error', function (error) {
+            console.log('Error is: ', error);
+        });
     } catch (error) {
         console.error(`Error: ${error.message}`);
         process.exit(1)
