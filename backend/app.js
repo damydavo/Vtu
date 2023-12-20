6e27302e25e27c6f9078e57cdda8b91701c52025
unknown <damydavo2015@gmail.com>
2023-12-20 11:51:45 +0100

    added the project

--- backend/app.js
+++ backend/app.js
@@ -21,9 +21,9 @@ app.use('/api/data', dataRoute)
 
 if (process.env.NODE_ENV === "production") {
     const path = require("path");
-    app.use(express.static(path.resolve(__dirname, 'client', 'build')));
+    app.use(express.static(path.resolve(__dirname, 'frontend', 'build')));
     app.get("*", (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'), function (err) {
+        res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'), function (err) {
             if (err) {
                 res.status(500).send(err)
             }
