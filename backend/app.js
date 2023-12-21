04e0ec004cc193ce37faa11f2730626c5f9ca810
unknown <damydavo2015@gmail.com>
2023-12-21 14:19:12 +0100

    added

--- backend/app.js
+++ backend/app.js
@@ -25,7 +25,7 @@ app.use((req, res, next) => {
 
 // Handle root path ("/") by redirecting to "/api/users"
 app.get('/', (req, res) => {
-    res.redirect('/api/users');
+    res.redirect('/');
 });
 
 app.use('/api/users', userRoute);
