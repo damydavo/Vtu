575a9b39374bef1a956d6928b2f3b90587c1a63b
unknown <damydavo2015@gmail.com>
2023-12-21 11:24:12 +0100

    added

--- backend/app.js
+++ backend/app.js
@@ -4,38 +4,44 @@ import path from 'path';
 import { notFound, errorHandler } from './middleWare/errorMiddleware.js';
 import connectDB from './config/db.js';
 
-const port = process.env.PORT || 8000
+const port = process.env.PORT || 8000;
 
-import userRoute from './routes/userRoute.js'
-import dataRoute from './routes/dataRoute.js'
+import userRoute from './routes/userRoute.js';
+import dataRoute from './routes/dataRoute.js';
 
 const app = express();
-app.use(express.static('public'));
-app.use(express.json())
-app.use(express.urlencoded({ extended: true }))
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-dotenv.config()
-connectDB()
+dotenv.config();
+connectDB();
 
 app.use((req, res, next) => {
     if (req.originalUrl && req.originalUrl.split("/").pop() === 'favicon.ico') {
         return res.sendStatus(204);
     }
-
     return next();
 });
 
+app.use('/api/users', userRoute);
+app.use('/api/data', dataRoute);
 
-app.use('/api/users', userRoute)
-app.use('/api/data', dataRoute)
+if (process.env.NODE_ENV === 'production') {
+    // Serve static files from the 'frontend/build' directory
+    app.use(express.static(path.join(__dirname, 'frontend/build')));
 
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(process.cwd(), 'frontend/build')));
-    app.get('*', (req, res) => res.sendFile(path.join(process.cwd(), 'frontend/build/index.html')));
-
+    // All other routes should be handled by the frontend application
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend/build/index.html'));
+    });
 }
 
 app.use(notFound);
-app.use(errorHandler)
+app.use(errorHandler);
 
-app.listen(port, () => console.log(`app is listening on port ${port}`));
+export default app;
+
+// For local development
+if (process.env.NODE_ENV !== 'test') {
+    app.listen(port, () => console.log(`App is listening on port ${port}`));
+}
