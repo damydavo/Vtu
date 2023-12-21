75e49da5cc8bfa5700b69d43e8a0c20d909fb0c2
unknown <damydavo2015@gmail.com>
2023-12-21 02:32:48 +0100

    added

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,6 @@ const app = express();
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-
 dotenv.config()
 connectDB()
 
@@ -25,7 +24,7 @@ if (process.env.NODE_ENV === "production") {
 
     app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', build, 'index.html'))
 }
-else {
+else { 
     app.get('/', (req, res) => {
         res.status(200).json({ message: "Welcome to VTU application" })
     })
