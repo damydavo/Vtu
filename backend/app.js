86263167c25571345f594c9b94b7a376dc8e9394
unknown <damydavo2015@gmail.com>
2023-12-21 03:03:30 +0100

    added

--- backend/app.js
+++ backend/app.js
@@ -21,10 +21,11 @@ app.use('/api/users', userRoute)
 app.use('/api/data', dataRoute)
 
 if (process.env.NODE_ENV === "production") {
-    app.use(express.static(path.join(__dirname, '../frontend/build')))
+    app.use(express.static(path.join(__dirname, '../frontend/build')));
 
-    app.get('*', (req, res) => res.sendFile(__dirname, '../', 'frontend', build, 'index.html'))
+    app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')));
 }
+
 else { 
     app.get('/', (req, res) => {
         res.status(200).json({ message: "Welcome to VTU application" })
