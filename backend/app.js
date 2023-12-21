4aea561eaad64bad6724487ad627b9e71b4d8203
unknown <damydavo2015@gmail.com>
2023-12-21 03:21:56 +0100

    added

--- backend/app.js
+++ backend/app.js
@@ -35,12 +35,6 @@ if (process.env.NODE_ENV === "production") {
     app.get('*', (req, res) => res.sendFile(path.join(__dirname, '../frontend/build/index.html')));
 }
 
-else { 
-    app.get('/', (req, res) => {
-        res.status(200).json({ message: "Welcome to VTU application" })
-    })
-}
-
 app.use(notFound);
 app.use(errorHandler)
 
